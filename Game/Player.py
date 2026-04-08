47d3ce00f5c6d530d9bbea1ad5f853d47f8467cd
kerubo <mongaremetrine5@gmail.com>
2026-04-08 16:55:47 +0300

    players

---
+++ Game/Player.py
@@ -0,0 +1,8 @@
+class Player():
+    def __init__(self,type="pc", cards=[],total_amount_bet=0,name="",amount=0):
+        
+       self.name=name
+       self.type=type
+       self.cards=cards
+       self.total_amount_bet=total_amount_bet
+       self.amount=amount
