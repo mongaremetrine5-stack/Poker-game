7e26dbe90000dc756f1e48bbd776ea4d9d87262a
kerubo <mongaremetrine5@gmail.com>
2026-04-13 00:10:48 +0300

    updated

--- Game/Player.py
+++ Game/Player.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 class Player():
     def __init__(self,type="pc", cards=[],total_amount_bet=0,name="",amount=0):
         
@@ -6,3 +9,47 @@ class Player():
        self.cards=cards
        self.total_amount_bet=total_amount_bet
        self.amount=amount
+       
+    def place_initial_bet(self):
+        
+        while True:
+            amount=input(f"Place initial bet amount. Current amount is {self.amount}:")
+            if amount.isdigit():
+                n=int(amount):
+                if n>0 and n<= self.amount:
+                    self.amount=self.amount-n #use a setter
+                    return n
+                
+                print("Invalid amount entered.")
+                print(f"Amount should be between 1 and {self.amount}.")
+                print("Please try again.")
+            else:
+                print("Please enter a valid number between 1 and {self.amount}.")
+                
+    def auto_match_or_auto_raise(self,amount):
+        print("PC thinking.What to do")
+        time.sleep(2)
+        to_do=random.randint(1,2)
+        raise_amount=amount+random.randint(10,250)
+        
+        if raise_amount>self.amount:
+            to_do=1
+            
+        if to_do==1:
+            if self.amount>amount:
+                self.amount-amount
+                print(f"Matching your action. Bet {amount}")    
+                return amount
+            else:
+                return"l"
+            
+            self.amount=self.amount-raise_amount
+            print("I have a good feeling. I raise by ",raise_amount)
+            return raise_amount
+        
+    def update_amount_bet(self,amount):
+        self.total_amount_bet=self.total_amount_bet+amount
+        
+    def reset_amount_bet(self):
+        self.total_amount_bet=0        
+            
