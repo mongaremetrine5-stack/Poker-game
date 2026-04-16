ad8909e48c5a492367710b2871022264e5e123eb
kerubo <mongaremetrine5@gmail.com>
2026-04-16 19:35:37 +0300

    changes

--- Game/Card.py
+++ Game/Card.py
@@ -1,36 +1,37 @@
-class Card ():
-    
-    RANKS = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
-    SUITES = ["HEART", "DIAMOND", "SPADE", "CLUBS"]
-    def __init__(self, suite, rank):
-       
+class Card():
+
+    RANKS=["A","K","Q","J","10","9","8","7","6","5","4","3","2"]
+    SUITES=["HEART","DIAMOND","SPADE","CLUBS"]
+    def __init__(self,suite,rank):
+
        
        if not isinstance(suite,str):
            raise TypeError(f"Suite expected to be a string got {type(suite).__name__}")
-       if not isinstance(rank,str):
-           raise TypeError(f"Rank expected to be a string got {type(rank).__name__}")
        
-       suiteUpper = suite.upper()
-       rankUpper = rank.upper()
+       if not isinstance(rank,str):
+          raise TypeError(f"Suite expected to be a string got {type(rank).__name__}")
+
+       suiteUpper=suite.upper()
+       rankUpper=rank.upper()
        if rankUpper in Card.RANKS:
            pass
        else:
-           raise TypeError(f"Added rank not in rank list{Card.RANKS}")
+           raise TypeError(f"Added rank not in rank list {Card.RANKS}")
+       
        if suiteUpper in Card.SUITES:
            pass
        else:
-           raise TypeError(f"Added suite not in suite list{Card.SUITES}")
-     
-       self.rank = rankUpper
-       self.suite = suiteUpper
+           raise TypeError(f"Added suite not in suite list {Card.SUITES}")
        
-    def printCard(self):
-        print("Rank", self.rank)
-        print("Suite", self.suite)
-        
-if __name__ == "__main__":
-    card1 = Card(suite="HEART", rank="A")
-    card1.printCard() 
-    
-    card2 = Card(suite="CLUBS", rank="3")
-    card2.printCard()
+       self.rank=rankUpper
+       self.suite=suiteUpper
+
+    def print_card(self):
+        print(f"{self.rank} {self.suite}")
+
+if __name__=="__main__":
+    card1=Card(suite="Joker",rank="A")
+    card1.print_card()
+
+    card2=Card(suite="Clubs",rank="3")
+    card2.print_card()
--- Game/Deck.py
+++ Game/Deck.py
@@ -1,69 +1,88 @@
 from Card import Card
 import random
 
-class Deck ():
-    
+class Deck():
+
     def __init__(self):
-        
-        ranks = Card.RANKS
-        suites = Card.SUITES
-        deck=[]# datatype is an array of card objects
-        
+
+        ranks=Card.RANKS
+        suites=Card.SUITES
+        deck=[]#datatype is an array
+
+
         for rank in ranks:
             for suite in suites:
-                card = Card(suite=suite, rank=rank)
+                card=Card(suite=suite,rank=rank)
                 deck.append(card)
-        self.deck = deck
-   
+        self.deck=deck
+
+    
     def shuffle(self):
         newDeck=[]
         deck=self.deck
         while True:
-            if len(deck) ==1:
+            if len(deck)==1:
                 card=deck[0]
                 newDeck.append(card)
                 break
-            n=random.randint(0, len(deck)-1)
-            
-            card=deck[n]
+            n=random.randint(0,len(deck)-1)
+       
+            card=deck[n] 
             deck.pop(n)
             newDeck.append(card)
-            self.deck=newDeck
-            
+        self.deck=newDeck
+
     def print_deck(self):
-        print=self.deck
-        print("After burning")   
-        top_card=self.deck[0]
-        self.deck.pop(0)
-        self.deck.append(top_card)
-        self.print_deck()   
-        pass     
-   
+        deck=self.deck
+        print("Deck size is",len(deck))
+        print("..............")
+        for card in deck:
+            card.print_card()
+            print("-------------")
+
     def burn_card(self):
-        # take top card puts it below the deck
-        print("before burning deck")
-        self.print_deck()
-        print("after burning deck")
+        #print("before buring deck")
+        #self.print_deck()
+        #print("After buring")
         top_card=self.deck[0]
         self.deck.pop(0)
         self.deck.append(top_card)
-        self.print_deck()
+        #self.print_deck()
+        pass
+    ##
+
     def give_card(self):
-        # take a card out of the deck from the end deck and give to player
+        #->take a card out of the deck from the end deck 
+        #and give it out
         top_card=self.deck[0]
         self.deck.pop(0)
         return top_card
-        pass
-    
-if __name__ == "__main__":
-        d1= Deck()
-        d1.shuffle()
-        #d1.burn_card()
-        card=d1.give_card()
-        print("card given")
-        card.printCard()
-        d1.print_deck()
         
+if __name__=="__main__":
+    d1=Deck()
+    d1.shuffle()
+    human_cards=[d1.give_card(),
+                 d1.give_card(),
+                 d1.give_card(),
+                 d1.give_card(),
+                d1.give_card(),
+                d1.give_card(),
+                d1.give_card()
+                 ]
 
-        
-                
+    for card in human_cards:
+        card.print_card()
+
+    royal=["A","K","Q","J","10"]
+
+    sorted_cards=[]
+
+    for rank in royal:
+        for card in human_cards:
+            if card.rank==rank:
+                sorted_cards.append(card)
+
+    print("Sorted cards")
+    for card in sorted_cards:
+        card.print_card()
+
